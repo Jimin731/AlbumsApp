bf085b1576a9393dd9bca40384f97332c8402a01
Jimin731 <petitestan@naver.com>
2021-07-08 11:48:24 +0900

    finish

--- AlbumsApp/ContentView.swift
+++ AlbumsApp/ContentView.swift
@@ -9,6 +9,16 @@ extension Album: Identifiable {
     var id: String { albumTitle }
 }
 
+extension Color{
+    static func argb(_ argb: UInt32) -> Color{
+        let a = Double(argb >> 24 & 0xFF) / 255.0
+        let r = Double(argb >> 16 & 0xFF) / 255.0
+        let g = Double(argb >> 8 & 0xFF) / 255.0
+        let b = Double(argb & 0xFF) / 255.0
+        return Color(.sRGB, red: r, green: g, blue: b, opacity: a)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         List {
@@ -29,17 +39,47 @@ struct ContentView_Previews: PreviewProvider {
 
 struct AlbumCell: View {
     let album: Album
+    @State var image: Image?
     var body: some View {
         HStack {
-            ImageStore.load(strUrl: album.image)
-            VStack {
+//            ImageStore.load(strUrl: album.image)
+            loadAlbumImage()
+                .resizable()
+                .frame(width: 56, height: 56, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            VStack(alignment: .leading) {
                 Text(album.albumTitle)
-                    .font(.subheadline)
-                    .foregroundColor(Color.blue)
-                    .multilineTextAlignment(.leading)
+                    .font(.headline)
+                    .foregroundColor(.argb(0xFF283747))
+//                    .foregroundColor(Color.blue)
+                    .frame(height: 50)
                 Text(album.artistName)
+                    .font(.caption)
+                    .foregroundColor(.argb(0xFF333333))
                     .multilineTextAlignment(.leading)
+                    .lineLimit(1)
             }
+        }
+    }
+    func loadAlbumImage() -> Image {
+        if image != nil{
+            return image!
+        }
+//        if let image = image{
+//            return image
+//        }
+        return ImageStore.load(strUrl: album.image) { image in
+            self.image = image
+        }
+    }
+}
+
+
+struct AlbumCell_Previews : PreviewProvider {
+    static var previews : some View {
+        List{
+            AlbumCell(album : Album(albumTitle : "HELLO1", artistName: "artist1", image : "url1"))
+            AlbumCell(album : Album(albumTitle : "HELLO2", artistName: "artist2", image : "url2"))
+            AlbumCell(album : Album(albumTitle : "HELLO3", artistName: "artist3", image : "url3"))
         }
     }
 }
--- AlbumsApp/ImageStore.swift
+++ AlbumsApp/ImageStore.swift
@@ -8,23 +8,40 @@ import UIKit
 import SwiftUI
 
 class ImageStore {
-    static func load(strUrl: String) -> Image {
+    static func load(strUrl: String, onDownload: @escaping (Image) -> Void) -> Image {
         guard let url = URL(string: strUrl) else {
             print("Invalid URL:\(strUrl)")
-            return Image(systemName: "wifi")
+            return Image(systemName: "tv.music.note")
+        }
+        NSLog("start: \(strUrl)")
+        OperationQueue().addOperation {
+            guard let data = try? Data(contentsOf: url) else {
+                print("Failed to load from \(strUrl)")
+                return
+            }
+            guard let uiImage = UIImage(data: data) else {
+                print("Not an image data: \(strUrl)")
+                return
+            }
+            let image = Image(uiImage: uiImage)
+            NSLog("end: \(strUrl)")
+            onDownload(image)
         }
         
-        guard let data = try? Data(contentsOf: url) else {
-            print("Failed to load from \(strUrl)")
-            return Image(systemName: "wifi.slash")
-        }
+//        guard let data = try? Data(contentsOf: url) else {
+//            print("Failed to load from \(strUrl)")
+//            return Image(systemName: "tv.music.note")
+//        }
+//
+//        guard let uiImage = UIImage(data: data) else {
+//            print("Not an image data: \(strUrl)")
+//            return Image(systemName: "tv.music.note")
+//        }
         
-        guard let uiImage = UIImage(data: data) else {
-            print("Not an image data: \(strUrl)")
-            return Image(systemName: "wifi")
-        }
         
-        print("Image loaded: \(strUrl)")
-        return Image(uiImage: uiImage)
+//
+//        print("Image loaded: \(strUrl)")
+//        return Image(uiImage: UIImage)
+        return Image(systemName: "tv.music.note")
     }
 }
